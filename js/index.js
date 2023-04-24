d61b2e75456d8bcacd52f23a5edae4a6fb1bc952
Guilherme <101355723+guilhermelike@users.noreply.github.com>
2023-04-24 17:03:04 -0300

    service page

--- js/index.js
+++ js/index.js
@@ -1,20 +1,10 @@
-let i = 0;
-let img = [];
-let slideTime = 5000; 
+var docWidth = document.documentElement.offsetWidth;
 
-img[0] = 'img/adv1.jpg';
-img[1] = 'img/adv2.jpg';
-img[2] = 'img/adv3.jfif';
-
-const changePicture = () => {
-    document.getElementById('container-banner').style.backgroundImage = "url(" + img[i] + ")";
-
-    if (i < img.length - 1) {
-        i++;
-    } else {
-        i = 0;
+[].forEach.call(
+  document.querySelectorAll('*'),
+  function(el) {
+    if (el.offsetWidth > docWidth) {
+      console.log(el);
     }
-    setTimeout(changePicture, slideTime);
-};
-
-$(document).ready(changePicture());
+  }
+);
